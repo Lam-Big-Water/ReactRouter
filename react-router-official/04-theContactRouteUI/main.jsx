d16dff6e9e82622af473c6921800198da5c55c2f
Sam Lam <xlin67948gmail.com>
2024-12-04 00:48:43 +0800

    next Data Writes

---
+++ react-router-official/04-theContactRouteUI/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Root from "./routes/root";
+
+import './index.css'
+import ErrorPage from './error-page';
+import Contact from './routes/contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
